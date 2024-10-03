9f249f305426153a23019a5f0961e6d3161ff79d
hardik21mishra <hardik.mishra2022@gmail.com>
2024-10-03 19:35:43 +0530

    just revised selection sort code

--- L16-Selection-Sort/basic.cpp
+++ L16-Selection-Sort/basic.cpp
@@ -2,17 +2,8 @@
 using namespace std;
 // ques -----> [6, 2, 8, 4, 10]
 // ans ------> [2, 4, 6, 8, 10]
-int main()
-{
-    int n,i, arr[100];
-    cout << "Enter Size of array: ";
-    cin >> n;
-    cout << endl;
-    cout << "Enter elemrnts in your array: " << endl;
-    for (int x = 0; x < n; x++)
-    {
-        cin >> arr[x];
-    }
+
+void SelectionSort(int arr[], int n){
     for (int i = 0; i < n - 1; i++)
     {
         int minIndex = i;
@@ -26,6 +17,21 @@ int main()
         }
     swap(arr[minIndex], arr[i]);
     }
+}
+int main()
+{
+    int n,i, arr[100];
+    cout << "Enter Size of array: ";
+    cin >> n;
+    cout << endl;
+    cout << "Enter elements in your array: " << endl;
+    for (int x = 0; x < n; x++)
+    {
+        cin >> arr[x];
+    }
+
+SelectionSort(arr, n);
+    
 cout<<"the sorted array: "<<endl;
 for (int i = 0; i < n ; i++)
     {
